2cb4a4eb755829f5495377f904cf6e30124cb898
Austin Hanson <berdon@gmail.com>
2015-11-23 08:46:58 -0600

    Fixing issue where masking doesn't apply on updated ng-model

--- angular-jquery-maskedinput.js
+++ angular-jquery-maskedinput.js
@@ -4,8 +4,16 @@ angular.module('angular-jquery-maskedinput', []).directive('mask', function () {
         restrict: 'A',
         link: function (scope, elem, attr, ctrl) {
 
-            if (attr.mask)
+            if (attr.mask) {
                 elem.mask(attr.mask, { placeholder: attr.maskPlaceholder });
+            }
+
+            if (attr.ngModel) {
+	            scope.$watch(attr.ngModel, function (v) {
+	            	// Force remasking on model changes
+	            	elem.mask(attr.mask, { placeholder: attr.maskPlaceholder });
+	            });
+	        }
         }
     };
 });
